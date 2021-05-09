d1c5547a01fb6544621674884b0e2356c9766c56
jaydavis359 <jaydavis_1@hotmail.com>
2021-05-09 20:29:06 +0000

    create assets sub folders

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+            var map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 15,
+                center: {
+                    lat: 51.89835739651741,
+                    lng: -8.413001888198336
+                }
+            });
+        } 
