fdc5a15b4b41f8955c96116a2e95e533d222e46a
jaydavis359 <jaydavis_1@hotmail.com>
2021-05-09 20:52:34 +0000

    move map script to js file

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,11 @@
+// Google Maps Integration with info from Code Institute and Traversy Media Youtube Channel
+
 function initMap() {
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 15,
-                center: {
-                    lat: 51.89835739651741,
-                    lng: -8.413001888198336
-                }
-            });
-        } 
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: {
+            lat: 51.89835739651741,
+            lng: -8.413001888198336
+        }
+    });
+}
