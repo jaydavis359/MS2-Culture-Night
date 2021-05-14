361165fba74d9732a424da0ed43f88a02790a2f1
jaydavis359 <jaydavis_1@hotmail.com>
2021-05-14 20:03:10 +0000

    move gallery script to its own page

---
+++ assets/js/gallery.js
@@ -0,0 +1,10 @@
+// Gallery Functionality Reference Traversy youtube channel
+
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+    current.src = e.target.src;
+}
