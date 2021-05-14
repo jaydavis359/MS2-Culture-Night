4f90767bbb86df2453763288ed26abff8184d8df
jaydavis359 <jaydavis_1@hotmail.com>
2021-05-14 11:12:50 +0000

    add response pop up to form submission

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -2,18 +2,18 @@
 // Form data email function created with info from course videos
 function sendMail(contactForm) {
     emailjs.send("gmail", "culture-night", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "booking_request": contactForm.tickets.value,
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "booking_request": contactForm.tickets.value,
+        })
+        // Response modified from CI course example
+        .then(
+            function (response) {
+                alert("Thanks for your request! We will be in touch shortly!", response);
+            },
+            function (error) {
+                alert("Your email request has failed to send, Please try again!", error);
+            }
+        );
+    return false; // To block from loading a new page
 }
-
